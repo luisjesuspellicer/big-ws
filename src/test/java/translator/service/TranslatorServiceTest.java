2a0342df5cd314910245206fefb088c0dca2f4ef
luisjesuspellicer <luis_jesus123@hotmail.com>
2015-10-28 14:12:31 +0100

    Update TranslatorServiceTest.java

--- src/test/java/translator/service/TranslatorServiceTest.java
+++ src/test/java/translator/service/TranslatorServiceTest.java
@@ -20,8 +20,8 @@ public class TranslatorServiceTest {
     TranslatorService translatorService;
     @Test
     public void translateTest() throws Exception {
-        TranslatedText translatedText = translatorService.translate("en", "es", "This is a test of translation service");
-        assertEquals("Esto es una prueba de servicio de traducción",translatedText.getTranslation());
+        TranslatedText translatedText = translatorService.translate("en", "es", "My name is Luis and I want finish");
+        assertEquals("Mi nombre es Luis y quiero terminar",translatedText.getTranslation());
     }
 
 }
